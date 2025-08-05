f3c05a1184736bc9f863a1c870c103ec71995119
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-08-05 18:33:55 +0700

    feat: add animations and React Bits components

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="top-0 fixed h-12 w-full mt-4">
-      <div className="backdrop-blur-sm z-50 border-2 border-purple-400 rounded-3xl flex w-full h-full justify-between items-center mx-auto max-w-xl px-6">
+    <nav className="top-0 fixed h-12 w-full mt-4 z-50">
+      <div className="backdrop-blur-sm border-2 border-purple-400 rounded-3xl flex w-full h-full justify-between items-center mx-auto max-w-xl px-6">
         <NavLink to="/" end>
           Home
         </NavLink>
--- app/pages/home/sections/welcome.tsx
+++ app/pages/home/sections/welcome.tsx
@@ -1,16 +1,57 @@
+import { useEffect } from "react";
+import Orb from "~/components/orb";
+import SplitText from "~/components/split-text";
+import gsap from "gsap";
+
 export default function Welcome() {
+  useEffect(() => {
+    gsap.fromTo(
+      "#fade-in",
+      { opacity: 0, y: 40 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        delay: 0.5,
+        ease: "power2.out",
+      },
+    );
+  }, []);
+
   return (
     <section
       id="welcome"
-      className="h-dvh w-full flex flex-col gap-4 items-center justify-center"
+      className="h-dvh w-full flex flex-col gap-4 items-center justify-center relative"
     >
-      <span className="text-8xl font-bold">Hello!</span>
-      <div>
-        <span className="text-3xl font-medium">I'm Valerian, a (future)</span>
-        &nbsp;
-        <span className="text-3xl font-medium bg-purple-400 px-1 py-0.5">
-          Web Developer
-        </span>
+      <Orb
+        hoverIntensity={0}
+        rotateOnHover={false}
+        hue={0}
+        forceHoverState={false}
+      />
+
+      <div className="absolute flex items-center justify-center flex-col gap-8">
+        <SplitText
+          text="Hello!"
+          className="text-8xl font-bold"
+          delay={100}
+          duration={0.6}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+        />
+
+        <div id="fade-in" className="opacity-0">
+          <span className="text-3xl font-medium">I'm Valerian, a (future)</span>
+          &nbsp;
+          <span className="text-3xl font-medium bg-purple-400 px-1 py-0.5">
+            Web Developer
+          </span>
+        </div>
       </div>
     </section>
   );
