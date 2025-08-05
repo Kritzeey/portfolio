38bb3651dfaf7d891e9419f9172713b9cc69ce30
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-08-05 22:17:25 +0700

    fix: fix animation

--- app/pages/about/sections/projects.tsx
+++ app/pages/about/sections/projects.tsx
@@ -17,7 +17,7 @@ export default function Projects() {
       {
         opacity: 1,
         y: 0,
-        duration: 1,
+        duration: 2,
         ease: "power2.out",
         scrollTrigger: {
           trigger: "#projects",
--- app/pages/about/sections/techs.tsx
+++ app/pages/about/sections/techs.tsx
@@ -17,7 +17,7 @@ export default function Techs() {
       {
         opacity: 1,
         y: 0,
-        duration: 1,
+        duration: 2,
         ease: "power2.out",
         scrollTrigger: {
           trigger: "#techs",
