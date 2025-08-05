f5468a06998d0356c66aed0350668baa71297969
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-08-05 18:02:59 +0700

    fix: minor formatting fix

--- app/pages/home/sections/welcome.tsx
+++ app/pages/home/sections/welcome.tsx
@@ -6,9 +6,9 @@ export default function Welcome() {
     >
       <span className="text-8xl font-bold">Hello!</span>
       <div>
-        <span className="text-2xl font-medium">I'm Valerian, a (future)</span>
+        <span className="text-3xl font-medium">I'm Valerian, a (future)</span>
         &nbsp;
-        <span className="text-2xl font-medium bg-purple-400 px-1 py-0.5">
+        <span className="text-3xl font-medium bg-purple-400 px-1 py-0.5">
           Web Developer
         </span>
       </div>
